77913b8534ec4b3d7767249e7dabdf407814704f
Blaine <blainevanderson@hotmail.com>
2013-05-05 12:54:18 -0700

    Updated code
    Updated code with feedback from community

--- src/binarySearch/BinarySearch.java
+++ src/binarySearch/BinarySearch.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BinarySearch {
 	
-	public int[] numbers = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
+	public int[] numbers = new int[] {1,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20}; 
 	
 	/**
 	 * @param num
@@ -17,30 +17,31 @@ public class BinarySearch {
 	 * function call.
 	 * 
 	 */
-	public int performSearch(int[] num, int key){
+	public int performSearch(int[] num, int key, int low, int high){
 		if(num.length == 0){
 			System.out.println("Array empty"); 
-			return 0; 
-		}else{
+			return -1; 
+		}else if((key < num[0])|| (key > num[num.length-1])){
+			System.out.println("Number not in bounds of array.");
+			return -1; 
+		}else if (low <= high){
+		
 			int mid; 
-			int number=0; 
-			mid = (num.length)/2; 
-			if(key == num[mid]){
-				number =  num[mid]; 
-				System.out.println("Found the number " + number); 
-				return number; 
-			}else if((key < num[mid]) && num.length > 1){
-				num = Arrays.copyOfRange(num, 0, mid); 
-				System.out.println("Low Range: " + Arrays.toString(num)); 
-				return performSearch(num, key); 
-			}else if((key > num[mid]) && num.length > 1){
-				num = Arrays.copyOfRange(num, mid, num.length); 
-				System.out.println("High Range: " + Arrays.toString(num)); 
-				return performSearch(num, key); 
+			mid = (low + high)/2; 
+			if(key < num[mid]){
+				high = mid - 1; 
+				return performSearch(num, key, low, high); 
+			}else if(key > num[mid]){
+				low = mid + 1; 
+				return performSearch(num, key, low, high); 
 			}else{
-				System.out.println("Number does not exist in array."); 
-				return 0; 
+				
+				System.out.println("Found the number in the array at index: " + mid); 
+				return mid; 
 			}
+		}else {
+			System.out.println("Number not in array."); 
+			return -1; 
 			//return number; 
 		}
 		
@@ -50,10 +51,12 @@ public class BinarySearch {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int key = 22; 
+		int key = 14; 
 		BinarySearch bs = new BinarySearch(); 
-		int index = bs.performSearch(bs.numbers, key); 
-		System.out.println("Number " + index);
+		int low = 0; 
+		int high = bs.numbers.length; 
+		bs.performSearch(bs.numbers, key, low, high); 
+		
 	}
 
 }
